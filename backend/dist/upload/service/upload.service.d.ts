4783928e6b7e73629af853fba0f51382d45d3c05
ch.mao <ch.mao@chmao-Mac.local>
2020-05-14 09:12:13 +0800

    test

--- backend/dist/upload/service/upload.service.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-export declare class UploadService {
-    constructor();
-    uploadQiuNiuFile(fileData: {
-        data: string | Buffer;
-        name?: string;
-    }): Promise<{}>;
-}
